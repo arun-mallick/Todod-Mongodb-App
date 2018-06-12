ae25de6431a2b6915565b201338bb1ee7cb1013f
apple <apple@apples-MacBook-Pro.local>
2018-06-12 23:28:49 +0530

    heroku mongo app 4

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/Arun');
-mongoose.connect('mongodb://arunslaz@gmail.com:1Situn@tcs4@ds257640.mlab.com:57640/sample');
+mongoose.connect('mongodb://admin:admin1234@ds257470.mlab.com:57470/todoapp');
 
 
 module.exports = {
