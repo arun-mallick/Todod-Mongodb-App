70d45e1d2934ae16c12e64cc50cd4d36dffac816
apple <apple@apples-MacBook-Pro.local>
2018-06-12 23:01:21 +0530

    heroku mongo app 1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Arun');
+//mongoose.connect('mongodb://localhost:27017/Arun');
+mongoose.connect('mongodb://mallick:1Situn@tcs4@ds257640.mlab.com:57640/sample');
+
 
 module.exports = {
     mongoose
